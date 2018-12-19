66555355df5c6688ba6497feb0b1521977d14aa3
ryo116 <wizacar.chel.0423@gmail.com>
2018-12-19 17:10:49 +0900

    Initial commit

---
+++ app/src/main/java/com/enes/ph_tester/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.enes.ph_tester
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import org.opencv.android.OpenCVLoader
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(!OpenCVLoader.initDebug()){
+            Log.i("OpenCV", "Failed")
+        }else{
+            Log.i("OpenCV", "successfully built !")
+        }
+    }
+}
